5882a6a4d96d0173c1e94d8264028971c65b65e7
zegabr <jgsp2@cin.ufpe.br>
2017-12-03 21:40:33 -0300

    Now I know how to get the text from a file and transform it to Strings

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
+var fs = require("fs")
 var a = "hue hueh hue"
 for (let i = 1; i <= 10; i++) {
     console.log(i)
-    
 }
-console.log(a)
+
+var text = fs.readFileSync("workspace/ic/hole1.cnf",'utf8')
+console.log(text)
