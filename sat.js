4db429550772236ba686002b429e1a6644eb1784
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-10-26 22:50:24 -0300

    Update sat.js
    Now is possible to choose which file to run, and optimization at console.log()

--- sat.js
+++ sat.js
@@ -1,23 +1,28 @@
 const fs = require("fs")
 const sat = require("./sat_modelo.js")
 
-filename = "hole4.cnf"//change filename here
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-console.log("\n\n\nSolving "+filename+"...\n")
+var filename = ""
 
-console.time("TIME TO SOLVE")
-const result = sat.solve(filename)
-console.timeEnd("TIME TO SOLVE")
-
-
-if(result!=null){
-  if(result.isSat==false){
-    console.log("\nTHIS PROBLEM IS INSATISFIABLE")
-  }else{
-    console.log("SOLUTION = ")
-    console.log(result.satisfyingAssignment)
+readline.question(`What .cnf file do you want?`, (filename) => {
+  console.log("\n\n\nSolving " + filename + "...\n")
+  
+  console.time("TIME TO SOLVE")
+  const result = sat.solve(filename)
+  console.timeEnd("TIME TO SOLVE")
+  
+  if(result!=null){
+    if(result.isSat==false){
+      console.log("\nTHIS PROBLEM IS INSATISFIABLE")
+    } else {
+      console.log("SOLUTION = " + result.satisfyingAssignment)
+    }
+  } else {
+    console.log("\nTHE PROBLEM SPECIFICATION DOESN'T CHECK WITH THE PARAMETERS")
   }
-
-}else{
-  console.log("\nTHE PROBLEM SPECIFICATION DOESN'T CHECK WITH THE PARAMETERS")
-}
+  readline.close()
+})
